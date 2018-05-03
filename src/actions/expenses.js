2eaadb7e17c0a0a92d2ce57a5a09e91038125ee2
Hunter Chang <hunter@madwiremedia.com>
2018-05-03 11:39:07 -0600

    fetch from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -11,7 +11,7 @@ export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const { description = '', note = '', amount = 0, createdAt = 0 } = expenseData;
     const expense = { description, note, amount, createdAt }
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -32,3 +32,24 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+        const expensesArray = [];
+        snapshot.forEach((childSnapshot) => {
+            expensesArray.push({
+                id: childSnapshot.key,
+                ...childSnapshot.val()
+            })
+        })
+        dispatch(setExpenses(expensesArray))
+    })
+  }
+};
